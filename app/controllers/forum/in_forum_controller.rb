52f83002cff1efe0d4f3e12e922aea49a969f7ad
jonybang <jonybang@mail.ru>
2015-11-24 16:23:02 +1000

    news for not login user

--- app/controllers/forum/in_forum_controller.rb
+++ app/controllers/forum/in_forum_controller.rb
@@ -10,10 +10,14 @@ class Forum::InForumController < InheritsController
     @user_id ||= session[:user_id]
   end
   def cur_person
-    @cur_person ||= Person.find user_id
+    @cur_person ||= Person.where(id: user_id)
   end
   def cur_person_role
-    @cur_person_role ||= cur_person.role_in_forum forum
-    session[:user_role] ||= @cur_person_role
+    if cur_person
+      @cur_person_role ||= @cur_person.role_in_forum forum
+      session[:user_role] ||= @cur_person_role
+    else
+      session[:user_role] ||= 'visitor'
+    end
   end
 end
