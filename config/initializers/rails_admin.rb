327087ecc0ea90b7ab4dc9c631670ffe92f6d9a7
jonybang <jonybang@mail.ru>
2015-12-07 14:48:18 +1000

    rails admin News description - now textarea

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,4 +4,14 @@ RailsAdmin.config do |config|
   config.authorize_with do
     redirect_to '/login' unless current_user && current_user.is_admin?
   end
+
+  config.model 'News' do
+    edit do
+      field :name
+      field :description, :text do
+        required true
+      end
+      include_all_fields
+    end
+  end
 end
