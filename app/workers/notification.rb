260248e9565dc80ff210c6b239836efb7618fa8c
jonybang <jonybang@mail.ru>
2015-12-09 17:02:13 +1000

    pushall config

--- app/workers/notification.rb
+++ app/workers/notification.rb
@@ -8,16 +8,16 @@ class Notification
 
   def push_all_request(data)
     post_request('https://pushall.ru/api.php', {
-        type: 'self',
-        id: '10507',
-        key: '0c1c4e330c1a464b3562a68715ffdfee',
+        type: 'broadcast',
+        id: '845',
+        key: 'be1acda5e707166d57930063e647cc7a',
         title: data[:title],
         text: data[:text]})
   end
   def post_request(url, data)
     uri = URI(url)
     http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
+    #http.use_ssl = true
 
     request = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
     request.body = data.to_json
