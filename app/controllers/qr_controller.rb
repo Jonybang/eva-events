20c7dcbde3f15fec871609c628a8d45c94e4c00b
jonybang <jonybang@mail.ru>
2015-12-12 11:00:25 +1000

    qr

--- app/controllers/qr_controller.rb
+++ app/controllers/qr_controller.rb
@@ -4,7 +4,8 @@ class QrController < ActionController::Base
       @resource = (params[:class].slice(0,1).capitalize + params[:class].slice(1..-1)).classify.constantize.find params[:id]
       @url = request.base_url + @resource.alias_url
       qr = RQRCode::QRCode.new( @url, :size => 5, :level => :h )
-      @qr_code = qr.to_img.resize(300, 300)
+      #@qr_code = qr.to_img.resize(300, 300)
+      @qr_code = qr.to_img.resize(700, 700)
     end
   end
 end
